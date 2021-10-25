354274ee0211925a30c435a46b52105a35a6548b
nobruin <nobruin@gmail.com>
2021-10-25 09:05:36 -0300

    add validation for math functions

--- src/main/kotlin/com/jaya/simpleexchange/util/ConversionUtil.kt
+++ src/main/kotlin/com/jaya/simpleexchange/util/ConversionUtil.kt
@@ -2,16 +2,27 @@ package com.jaya.simpleexchange.util
 
 
 import org.springframework.stereotype.Component
+import java.lang.RuntimeException
 import java.math.BigDecimal
 
 @Component
 class ConversionUtil {
 
     fun calculateConversionRate(currency: Double?, destinyCurrency: Double?): BigDecimal? {
+
+        when{
+            currency == null || currency <= 0.0 -> throw RuntimeException("values or Values is invalid for this operation")
+            destinyCurrency == null || destinyCurrency <= 0.0 -> throw RuntimeException("values or Values is invalid for this operation")
+        }
+
         return (destinyCurrency?.div(currency!!))?.toBigDecimal()
     }
 
     fun calculateAmount(amount: BigDecimal, rateConversion: BigDecimal?): BigDecimal {
-        return amount.let { rateConversion?.times(it) }!!
+
+        if(rateConversion == null || rateConversion <= "0.0".toBigDecimal()){
+            throw RuntimeException("value is invalid for this operation")
+        }
+        return amount.let { rateConversion.times(it) }
     }
 }
