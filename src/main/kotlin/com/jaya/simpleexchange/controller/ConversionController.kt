92fd9f46e0f6d4b40f24bc06452b22bde1704fdf
ext_brsalis <ext_brsalis@mercadolibre.com>
2023-07-09 15:26:33 -0300

    feature: remove more redundancies

--- src/main/kotlin/com/jaya/simpleexchange/controller/ConversionController.kt
+++ src/main/kotlin/com/jaya/simpleexchange/controller/ConversionController.kt
@@ -5,8 +5,6 @@ import com.jaya.simpleexchange.entity.Conversion
 import com.jaya.simpleexchange.service.ConversionService
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
-import io.swagger.annotations.ApiResponse
-import io.swagger.annotations.ApiResponses
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
